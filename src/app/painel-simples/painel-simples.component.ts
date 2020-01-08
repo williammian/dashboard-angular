a223e0713ecc73d18aac299eb9e893a17cc4d3d0
William Mian <william_mian@yahoo.com.br>
2020-01-08 17:08:37 -0300

    Input e ng-content

--- src/app/painel-simples/painel-simples.component.ts
+++ src/app/painel-simples/painel-simples.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-painel-simples',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./painel-simples.component.css']
 })
 export class PainelSimplesComponent implements OnInit {
+
+  @Input() titulo;
+  @Input() col;
+  @Input() tipo = "info";
 
   constructor() { }
 
