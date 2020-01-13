3f812ddd32b20cad6cf2b0a8089d2cdd0c2e9aaf
William Mian <william_mian@yahoo.com.br>
2020-01-13 17:22:27 -0300

    alteracao resumo

--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumoComponent implements OnInit {
 
+  resumo = {
+    consultas : {
+      consultas_30dias_anteiores : 87,
+      consultas_30dias_posteriores : 79
+    },
+    faturamento : {
+      anterior : {
+        valor : 100000,
+        comparativo : 19
+      },
+      previsao : {
+        valor : 90000,
+        comparativo : -10
+      }
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
