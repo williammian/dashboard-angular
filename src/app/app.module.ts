318a79bd872399208caa1bb857a0c587f57d0e64
williammian <william_mian@yahoo.com.br>
2020-01-13 06:35:38 -0300

    resumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelSimplesComponent,
-    BarraNavegacaoComponent
+    BarraNavegacaoComponent,
+    ResumoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/resumo/resumo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resumo',
+  templateUrl: './resumo.component.html',
+  styleUrls: ['./resumo.component.css']
+})
+export class ResumoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
