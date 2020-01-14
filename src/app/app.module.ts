95969952f0d385ccbe1cea30e51473ecfdd6be45
William Mian <william_mian@yahoo.com.br>
2020-01-14 11:19:58 -0300

    view consulta e ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 import { ResumoComponent } from './resumo/resumo.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelSimplesComponent,
     BarraNavegacaoComponent,
-    ResumoComponent
+    ResumoComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule
