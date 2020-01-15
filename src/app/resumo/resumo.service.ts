7df9a268fa23e47d0c69c18719f8569f3c22d9a2
William Mian <william_mian@yahoo.com.br>
2020-01-15 10:33:06 -0300

    criando variavel de ambiente

--- src/app/resumo/resumo.service.ts
+++ src/app/resumo/resumo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
 
 @Injectable()
 export class ResumoService {
@@ -7,6 +8,6 @@ export class ResumoService {
   constructor(private http:HttpClient) { }
 
   getResumo() {
-    return this.http.get("http://www.devup.com.br/php/api-dashboard/api/resumo");
+    return this.http.get(environment.apiUrl + "/resumo");
   }
 }
